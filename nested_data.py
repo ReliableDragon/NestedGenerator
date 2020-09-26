d89276ae766363a409e8a7d8c96e261459a2bcc8
Gabe Leonard <gabe@databaseguy.com>
2020-09-26 00:15:52 -0700

    Initial Commit
    Buncha fancy lists to generate less boring D&D NPCs. Over-engineered for amusement purposes.

---
+++ nested_data.py
@@ -0,0 +1,327 @@
+import re
+import random
+import uuid
+import math
+
+class WeightedChoice():
+    def __init__(self, weight, choice, tag=1):
+        self.weight = int(weight)
+        self.choice = choice
+        self.tag = tag
+
+    def __str__(self):
+        return f'({self.weight}) "{self.choice}"'
+
+    def __repr__(self):
+        return self.__str__()
+
+
+class NestedChoices():
+
+    def __init__(self, namespace_id, choices_tree={}):
+        self.namespace_id = namespace_id
+        self.choices = choices_tree
+        self.subtables = {}
+
+    def __str__(self):
+        val = '{\n'
+        indent = 2
+        val += recursive_dict_print(self.choices, indent)
+        val += '}'
+
+        return val
+
+    def __repr__(self):
+        return self.__str__()
+
+    @staticmethod
+    def load_from_file(filename):
+        with open(filename, 'r', encoding='utf-8') as choices_file:
+            choices_string = choices_file.read()
+            namespace_id = choices_string.split('\n', 1)[0]
+            #Strip the id off.
+            choices_string = choices_string.split('\n', 2)[2]
+            validate_choices(namespace_id, choices_string)
+            choices_tree = NestedChoices.choices_string_to_tree(choices_string)
+
+        return NestedChoices(namespace_id, choices_tree)
+
+    @staticmethod
+    def choices_string_to_tree(choices_string):
+        choices_tree = {}
+
+        top_level_choices_list = choices_string.split('\n\n')
+        top_level_choices_list = [choice.strip() for choice in top_level_choices_list]
+        for top_level_choice_data in top_level_choices_list:
+            indent = 0
+            parent_choicedict_stack = [choices_tree]
+
+            nested_choices = split_into_lines(top_level_choice_data)
+            top_level_choice = nested_choices.pop(0)
+
+            try:
+                parent = WeightedChoice(*top_level_choice.split(' ', 1))
+            except TypeError:
+                # Empty choice.
+                parent = WeightedChoice(top_level_choice, '')
+
+            choices_tree[parent] = {}
+            current_dict = choices_tree[parent]
+            parent_choicedict_stack.append(current_dict)
+            wc_tag = 1
+
+            for choice in nested_choices:
+                if is_tag_marker(choice):
+                    wc_tag += 1
+                    continue
+                # Check how many spaces there are to find the indent level.
+                new_indent = len(choice) - len(choice.lstrip(' '))
+                # If indent is not increased, the last value was not a parent.
+                if new_indent == indent:
+                    parent_choicedict_stack.pop()
+                    current_dict = parent_choicedict_stack[-1]
+                # The indent has decreased, and we need to remove the previous
+                # value, which was not a parent, as well as the parent before
+                # that, as we've travelled back up a level.
+                elif new_indent < indent:
+                    parent_choicedict_stack.pop()
+                    parent_choicedict_stack.pop()
+                    current_dict = parent_choicedict_stack[-1]
+
+                indent = new_indent
+                choice = choice[indent:]
+
+                try:
+                    weighted_choice = WeightedChoice(*choice.split(' ', 1), tag=wc_tag)
+                except TypeError:
+                    # Empty choice.
+                    weighted_choice = WeightedChoice(choice, '', wc_tag)
+                    print(weighted_choice)
+
+                current_dict[weighted_choice] = {}
+                current_dict = current_dict[weighted_choice]
+                parent_choicedict_stack.append(current_dict)
+
+        return choices_tree
+
+    @staticmethod
+    def load_from_string_list(namespace_id, choices, probs=[]):
+        if probs:
+            assert len(probs) == len(choices), 'Got a list of probabilities that was not the same length as the list of choices!'
+            choices = [str(prob) + ' ' + choice for prob, choice in zip(probs, choices)]
+        else:
+            choices = ['1 ' + s for s in choices]
+        choices_string = '\n\n'.join(choices)
+        validate_choices(namespace_id, choices_string)
+        choices_tree = NestedChoices.choices_string_to_tree(choices_string)
+
+        return NestedChoices(namespace_id, choices_tree)
+
+
+
+    def register_subtable(self, nc):
+        self.subtables[nc.namespace_id] = nc
+
+    # Given a nested choices string from get_choices, randomly generates a choice
+    # from it. Parameters can be passed to affect how it runs. 'num' says how many
+    # choices to generate, and 'uniqueness_level' says at what level the returned
+    # choices should be unique. -1 means the lowest level, simply don't repeat the
+    # exact same choice, while positive values starting from 1 indicate which level
+    # of indentation will be removed if a previous choice chose it.
+    def gen_choice(self, params={'num': 1, 'uniqueness_level': -1}):
+        generated_choices = []
+        used_choices = []
+
+        for i in range(params['num']):
+            generated = False
+            generated_choice = '$'
+            dict_and_choice_backtrace = []
+            level = 0
+            choices_dict = self.choices
+
+            # generated_choice = generation_step(generated_choice, dict_and_choice_backtrace, )
+            while not generated:
+                level += 1
+                filtered_choices = list(filter(lambda c: c not in used_choices, choices_dict))
+
+                weighted_choice = pick_choice(filtered_choices)
+
+                if level == params['uniqueness_level']:
+                    used_choices.append(weighted_choice)
+                    remove_childless_parents(dict_and_choice_backtrace, used_choices)
+
+                generated_choice = self.make_replacements(generated_choice, weighted_choice)
+                # If there's nothing in the corresponding list, we're at a leaf node
+                # and are done generating this choice.
+                if not choices_dict[weighted_choice]:
+                    if params['uniqueness_level'] == -1:
+                        used_choices.append(weighted_choice)
+                        remove_childless_parents(dict_and_choice_backtrace, used_choices)
+                    generated = True
+                else:
+                    dict_and_choice_backtrace.append((choices_dict, weighted_choice))
+                    choices_dict = choices_dict[weighted_choice]
+
+            generated_choices.append(generated_choice)
+
+        if params['num'] == 1:
+            return generated_choice
+        else:
+            return generated_choices
+
+    def make_replacements(self, generated_choice, weighted_choice):
+        # print(weighted_choice)
+        generated_choice = generated_choice.replace('$', weighted_choice.choice)
+        generated_choice = replace_ranges(generated_choice)
+        generated_choice = self.make_subtable_calls(generated_choice)
+
+        return generated_choice
+
+    def make_subtable_calls(self, generated_choice):
+        subtable_replace = re.compile('@([\w_]+)')
+        match = subtable_replace.search(generated_choice)
+        while match:
+            full_match = match.group(0)
+            subtable_id = match.group(1)
+
+            subtable = self.subtables[subtable_id]
+
+            generated_choice = generated_choice.replace(full_match, subtable.gen_choice())
+            match = subtable_replace.search(generated_choice)
+        return generated_choice
+
+def replace_ranges(generated_choice):
+    num_replace = re.compile('\[(\d+)-(\d+)(G|N)?\]')
+    match = num_replace.search(generated_choice)
+    while match:
+        full_match = match.group(0)
+        start = int(match.group(1))
+        end = int(match.group(2))
+        type = match.group(3)
+
+        val = None
+        if type == 'N':
+            # start = mean, end = stddev
+            val = math.floor(random.gauss(start, end))
+        elif type == 'G':
+            #start = alpha, end = beta
+            val = math.floor(random.gammavariate(start, end))
+        else:
+            val = random.randint(start, end)
+
+        generated_choice = generated_choice.replace(full_match, str(val))
+        match = num_replace.search(generated_choice)
+    return generated_choice
+
+def is_tag_marker(choice):
+    return re.match('(  )+\$', choice)
+
+def pick_choice(filtered_choices):
+    total_weight = sum([wc.weight for wc in filtered_choices])
+    assert total_weight > 0, "Total weight was <= 0, most likely you wrote a generation that removed all valid choices from a config."
+    rand = random.randint(1, total_weight)
+
+    for wc in filtered_choices:
+        rand -= wc.weight
+        if rand <= 0:
+            return wc
+
+# This one's a bit tricky. We take in a backtrace object which contains a trail of WCs
+# that we may have to remove, and the dictionaries that they occur in. Starting from the
+# bottom up, we look up the values under each WC in its parent dictionary, which lets us
+# check if there are any left uncovered. If there are, then we break. Otherwise, we will
+# use the dictionary to remove the WC, and go up another level to check again.
+def remove_childless_parents(dict_and_choice_backtrace, used_choices):
+    for dict, wc in dict_and_choice_backtrace[::-1]:
+        if len(list(filter(lambda d: d not in used_choices, dict[wc].keys()))) != 0:
+            break
+        used_choices.append(wc)
+
+def recursive_dict_print(dict, indent):
+    val = ''
+    indent_str = ' ' * indent
+    for k, v in dict.items():
+        # print(f'{k}: {v}')
+        val += indent_str + str(k) + ': {\n'
+        val += recursive_dict_print(v, indent + 2)
+        val += indent_str + '}\n'
+    return val
+
+def gen_choice_bak(choices, params={'num': 1, 'uniqueness_level': -1}):
+    validate_choices(choices)
+
+    generated = False
+    generated_choice = '$'
+    level = 0
+    while not generated:
+        weights_and_choices = [choice.split(' ', 1) for choice in choices]
+        weighted_choices = list(map(lambda wandp: (int(wandp[0]), wandp[1]), weights_and_choices))
+
+        total_weight = sum([i for i, _ in weighted_choices])
+        rand = random.randint(1, total_weight)
+
+        i = 0
+        while rand > 0:
+            rand -= weighted_choices[i][0]
+            if rand > 0:
+                i += 1
+        choice = weighted_choices[i][1]
+
+        if '$' not in choice:
+            generated_choice = generated_choice.replace('$', choice)
+            generated = True
+        else:
+            level += 2
+            choices = re.split(f'\r?\n {{{level}}}(?! )' , choice)
+
+            generated_choice = generated_choice.replace('$', choices[0])
+
+            choices = choices[1:]
+
+    return generated_choice
+
+def load_from_file_bak(filename):
+    with open(filename, 'r', encoding='utf-8') as choices_file:
+        choices_string = choices_file.read().split('\n\n')
+        choices_string = [choice.strip() for choice in choices_string]
+        # weights_and_choices = [choice.split(' ', 1) for choice in choices_string]
+        # weighted_choices = list(map(lambda wandp: (int(wandp[0]), wandp[1]), weights_and_choices))
+    return weighted_choices
+
+def validate_choices(namespace_id, choices_string):
+    assert len(choices_string) != 0, 'Got empty choices data!'
+    lines = choices_string.split('\n')
+    must_indent = False
+    prev_indent = 0
+    blank = False
+
+    for line in lines:
+        if line == '':
+            if blank:
+                raise Error(f'Found two blank lines in a row in {namespace_id}.')
+            blank == True
+            continue
+
+        blank == False
+        indent = len(line) - len(line.lstrip(' '))
+        if must_indent:
+            assert indent == prev_indent + 2, f'Line {line} in {namespace_id} did not indent, despite a symbol requiring indentation on the previous line.'
+            must_indent = False
+        else:
+            assert indent % 2 == 0 and indent < prev_indent + 2, f'Line {line} in {namespace_id} in  has an improper number of spaces.'
+
+        assert re.match(f' *\d+', line), f'Line {line} in {namespace_id} failed to match the expected format.'
+
+        if '$' in line and line != (' ' * indent) + '$':
+            must_indent = True
+
+        prev_indent = indent
+
+def split_into_lines(s):
+    return re.split('\r?\n', s)
+
+if __name__ == "__main__":
+    choices = NestedChoices.load_from_file('test_places.txt')
+    subtable = NestedChoices.load_from_string_list('countries_table', ['Germany', 'France', 'UK'], [5, 3, 1])
+    choices.register_subtable(subtable)
+    print(choices.gen_choice(params={'num': 5, 'uniqueness_level': -1}))
---
+++ random_npc.py
@@ -0,0 +1,80 @@
+#!/usr/bin/python
+
+import random as rand
+import math
+from nested_data import NestedChoices
+
+def create_character():
+  name_syllables = ['al', 'ben', 'cor', 'dan', 'fan', 'fer', 'frey', 'gra', 'gar', 'ger', 'hin', 'har',
+                    'par', 'pen', 'pul', 'ser', 'star', 'ston', 'nikov', 'wray', 'chill', 'chan', 'lon',
+                    'and', 'drak', 'crat', 'yon']
+  name = ''.join([name_syllables[rand.randint(0, len(name_syllables)-1)] for _ in range(0, rand.randint(2, 4))])
+  name = name.capitalize()
+
+  races = ['human', 'elf', 'dwarf', 'half-elf', 'gnome', 'halfling', 'half-orc', 'orc', 'goblin']
+  race_probs = [100, 10, 10, 20, 5, 5, 3, 1, 1]
+  race = rand.choices(races, weights=race_probs)[0]
+
+  gender = 'male' if rand.random() < 0.5 else 'female'
+
+  age = math.floor(rand.gammavariate(5, 8))
+  if race == 'elf':
+      age *= 10
+  elif race == 'dwarf':
+      age *= 3
+  elif race == 'goblin':
+      age //= 3
+
+  relationships = ['single', 'courting', 'engaged', 'married', 'widowed', 'divorced']
+  if age < 13:
+      relationship = 'single'
+  elif age < 18:
+      relationship = relationships[rand.randint(0, 1)]
+  elif age < 25:
+      relationship = relationships[rand.randint(0, 4)]
+  else:
+      relationship = relationships[rand.randint(0, len(relationships)-1)]
+
+  children = 0 if relationship in ['single', 'engaged'] else min(rand.randint(0, 6), age - 18 // 2)
+
+  jobs = ['butcher', 'baker', 'farmer', 'grocer', 'glassblower', 'blacksmith', 'tanner', 'clothier',
+          'leatherworker', 'florist', 'shepherd', 'hunter', 'woodsman', 'lumberjack', 'construction worker',
+          'priest', 'guard', 'guard', 'soldier', 'clerk', 'nobleman', 'barkeep', 'clockmaker', 'silversmith',
+          'potter', 'cafe proprietor', 'groundskeeper', 'Dolora thug', 'Corela thug', 'Straka thug']
+  nested_jobs = NestedChoices.load_from_string_list('random_jobs', jobs)
+  job = nested_jobs.gen_choice()
+  # job = jobs[rand.randint(0, len(jobs) - 1)]
+
+  wealths = ['dirt poor', 'dirt poor', 'poor', 'poor', 'getting by', 'getting by', 'getting by', 'well-off',
+            'well-off', 'rich']
+  wealth = rand.choice(wealths)
+
+  classes = ['commoner', 'guard', 'acolyte', 'mage', 'fighter', 'ranger', 'rogue', 'bard', 'sorcerer', 'wizard']
+  class_probs = [50, 25, 10, 5, 4, 2, 1, 1, 1, 1]
+  npc_class = rand.choices(classes, weights=class_probs)[0]
+  level = math.floor(rand.gammavariate(1, 3) + 1)
+
+  desires = ['wealth', 'power', 'love', 'fame', 'peace and quiet', 'adventure', 'security', 'an easy life',
+             'food', 'drink', 'friends', 'their family', 'revenge', 'piety', 'arcane knowledge']
+  desire_probs = [100, 50, 50, 30, 100, 30, 50, 50, 30, 50, 40, 70, 10, 25, 5]
+  desire = rand.choices(desires, weights=desire_probs)[0]
+
+  event_choices = NestedChoices.load_from_file('npc_events.txt')
+  event_choices.register_subtable(nested_jobs)
+  events = event_choices.gen_choice(params={'num':rand.randint(0, 3), 'uniqueness_level':1})
+  if not isinstance(events, list):
+      events = [events]
+
+  char = f'{name}, a {age} year(s) old {gender} {race}.\n'
+  char += f'They are {relationship}, and ' + ('don\'t have' if children == 0 else f'have {children}') + ' children.\n'
+  char += f'They are a {job}, and are {wealth}.\n'
+  char += f'Their greatest desire in life is {desire}.\n'
+  char += f'They are a level {level} {npc_class}.\n'
+
+  for event in events:
+      char += f'\n{event}.'
+
+  return char
+
+if __name__ == "__main__":
+  print(create_character())
