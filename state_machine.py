a32ae95bd683923000893898b5352ab31b8d6b95
Gabe Leonard <gabe@databaseguy.com>
2020-10-03 00:50:53 -0700

    State Machine Probably Works
    Finished up the state machine, I think. It's got a fair number of test cases, but I'm sure I'll find things that I've missed - it's late, and I'm not up to thinking up every edge case right now. Plus, state machines (especially with the janky nesting solution I'm using) have a LOT of edge cases.
    
    I'd still like to find  a better solution for handing how to identify which part of the string maps to which part of the automata in order to parse it, but I want to take a stab at figuring out a solution myself before I look up the "right" way to do it.
    
    Next steps: Writing the code to instantiate automata from the ABNF file, either extending the automata to a push down automata or finding another way to handle the non-context free parts of the grammar, and converting from the automata result into a parse tree. Hopefully once that's done the parsing will be more robust.

--- state_machine.py
+++ state_machine.py
@@ -1,6 +1,8 @@
 import logging
 import set_up_logging
 
+from collections import defaultdict
+
 FINAL_STATE = "FINAL"
 START_STATE = "START"
 
@@ -14,28 +16,57 @@ class Edge():
         self.dest = dest
 
     def __str__(self):
-        return f'{{input: "{self.input}", dest: {self.dest}}}'
+        return f'{{Edge | input: "{self.input}", dest: {self.dest}}}'
 
     def __repr__(self):
         return self.__str__()
 
-
+# START_STATE and FINAL_STATE may not have is_automata set to True.
 class State():
-
-    def __init__(self, id_, edges=[]):
+    def __init__(self, id_, edges=[], is_automata=False):
         self.id = id_
-        if not isinstance(edges, list):
+        if edges == None:
+            edges = []
+        elif not isinstance(edges, list):
             edges = [edges]
         for edge in edges:
             assert isinstance(edge, Edge), f'State {id_} got edge {edge} that was not an instance of class Edge!'
         self.edges = edges
+        self.is_automata = is_automata
 
     def __str__(self):
-        return f'State {self.id}: {[str(edge) for edge in self.edges]}'
+        return f'{{State {"(sub-automata) " if self.is_automata else ""}| {self.id}: {[str(edge) for edge in self.edges]}}}'
 
     def __repr__(self):
         return self.__str__()
 
+    def add_edge(self, edge):
+        assert isinstance(edge, Edge), f'State {id_} got edge {edge} that was not an instance of class Edge!'
+        self.edges.append(edge)
+
+# Holds information about calls to sub-automata. Start is inclusive, end is
+# exclusive.
+class StateRecord():
+    # (state_id: int or str, start: int, end: int, nested_path=[StateRecord]
+    def __init__(self, automata_id, state_id, start, end, nested_path=None):
+        self.automata_id = automata_id
+        self.id = state_id
+        self.start = start
+        self.end = end
+        self.nested_path = nested_path
+
+    def __str__(self):
+        val = f'StateRecord[{self.automata_id}] | state_id: "{self.id}" [{self.start}:{self.end}]'
+        if self.nested_path:
+            val += f' - {self.nested_path}'
+        return val
+
+
+    def __repr__(self):
+        return self.__str__()
+
+    def __eq__(self, other):
+        return self.id == other.id and self.start == other.start and self.end == other.end and self.nested_path == other.nested_path
 
 # State machine implementation for modified ABNF parsing. Records which pieces
 # of the input string matched where, and has only one starting and final state each.
@@ -45,11 +76,11 @@ class StateMachine():
     def __init__(self, id_=0, states=[]):
         self.id = id_
         for state in states:
-            assert isinstance(state, State), f'Got edge {state} that was not an instance of class Edge!'
-        self.state_map = {state.id: state.edges for state in states}
-        self.parsed_value = ""
-        self.sub_values = {}
-        self.stack = []
+            assert isinstance(state, State), f'Got state {state} that was not an instance of class State!'
+        self.state_map = {state.id: state for state in states}
+        self.nested_automata = {}
+        self.alternatives_map = defaultdict(lambda: [(START_STATE, 0, [])]) # Of the form {start: (state, str_pos, path)}
+        self.used_state_pos_map = defaultdict(list) # Of the form {start: (state, str_pos).
 
     def __str__(self):
         return f'State Machine {self.id}: {self.state_map}'
@@ -58,59 +89,116 @@ class StateMachine():
         return self.__str__()
 
     def add_state(self, state):
-        self.state_map[state.id] = state
-        if final:
-            self.final_states.append(state)
+        assert isinstance(state, State), f'Got state {state} that was not an instance of class State!'
+        assert state.id not in self.state_map.keys(), f'Attempted to add state {state} that was already present in state machine {self.id}!'
+        self.state_map[state.id] = state.edges
 
+    def add_edge(self, state_id, edge):
+        assert isinstance(edge, Edge), f'State {id_} got edge {edge} that was not an instance of class Edge!'
+        assert state_id in self.state_map.keys(), f'Attempted to add edge for nonexistent state {state} in state machine {self.id}!'
+        self.state_map[state.id].append(edge)
+
+    def register_automata(self, automata):
+        self.nested_automata[automata.id] = automata
+
+    # Returns found_bool
     def accepts(self, string):
-        logger.debug(f'Checking if machine {self} accepts "{string}".')
-        used_state_pos = [] # Of the form (state, str_pos).
-        alternatives = [(START_STATE, 0)]
+        return self._accepts(string, partial_match=False)[0]
+
+    # Returns (found: bool, path: list(StateRecord), end_pos: int)
+    def accepts_partial(self, string, start=0):
+        return self._accepts(string, partial_match=True)
+
+    def reset(self):
+        logger.debug('Resetting machine {self}')
+        self.alternatives_map = defaultdict(lambda: [(START_STATE, 0, [])])
+        self.path_map = defaultdict(list)
+        self.used_state_pos_map = defaultdict(list) # Of the form (state, str_pos).
+
+    def debug(self, string):
+        logger.debug(f'{self.id}: ' + string)
+
+    # TODO: Decide if this should to a more strict DFS, to obviate the need to copy the path all over.
+    # Returns (found_bool, path_state_record, end_pos)
+    def _accepts(self, string, start=0, partial_match=False):
+        self.debug(f'Checking if machine {self} accepts "{string}". ({"Partial match" if partial_match else "Full match"})')
+        used_state_pos = self.used_state_pos_map[start]
+        alternatives = self.alternatives_map[start]
 
         while True:
-            logger.debug(f'alternatives: {alternatives}')
+            self.debug(f'alternatives: {alternatives}')
 
             if len(alternatives) == 0:
-                logger.debug(f'Finished search, no accepting path found.')
-                return False
+                self.debug(f'Finished search, no accepting path found.')
+                return False, None, -1
 
-            state, i = alternatives.pop()
-            used_state_pos.append((state, i))
-            logger.debug(f'state id: {state}, edges: {self.state_map[state]}')
+            state_id, i, prev_path = alternatives.pop()
+            state = self.state_map[state_id]
 
-            for edge in self.state_map[state]:
+            used_state_pos.append((state_id, i))
+
+            # TODO: Look into making this properly DFS, so that we don't store
+            # so many copies of the path and can restore the below code.
+            # Remove path that we're no longer using, if we've backed up.
+            # Strict >, since end is exclusive. It can be the same as start though,
+            # which means we're up to a character but that path step did not process anything.
+            # while prev_path and prev_path[-1].end > i:
+            #     logger.debug(f'Popping path step {prev_path[-1]} from path.')
+            #     prev_path.pop()
+            # logger.debug(f'Evaluating alternative at state "{state_id}" ({state}), with path: {prev_path}')
+
+            # Making a call to a nested machine
+            if state.is_automata:
+                assert state_id in self.nested_automata.keys(), f'Got state {state} whose state_id {state_id} was not in nested_automata list {self.nested_automata.keys()}!'
+                # Note that the automata can be called multiple times.
+                accepted, nested_path, match_end = self.nested_automata[state_id].accepts_partial(string, start=i)
+                # nested_path = self.nested_automata[state_id].get_path(i)
+                prev_path.append(StateRecord(self.id, state_id + '_internal', i, match_end, nested_path))
+                if not accepted:
+                    # If the nested automata doesn't have any way to accept part of the string,
+                    # then we continue to skip investigating the edges coming out of it.
+                    continue
+                else:
+                    # The subautomata may be able to accept more, so we have to try it until
+                    # it returns a failure.
+                    alternatives.append((state_id, match_end, prev_path))
+                i = match_end
+
+
+            for edge in state.edges:
+                # Make a copy
+                path = list(prev_path)
                 input_length = len(edge.input)
-                logger.debug(f'edge: {edge}')
+                match_end = i + input_length
+                self.debug(f'Evaluating edge: {edge}')
 
-                # if not edge.input:
-                #     state_pos_pair = (edge.dest, i)
-                #     if state_pos_pair not in used_choices:
-                #         logger.debug(f'Appending {state_pos_pair} (equivalent to {(edge.dest, string[i+input_length:])})')
-                #         append_if_not_used(state_pos_pair, alternatives, used_values)
-                #     else:
-                #         logger.debug(f'Edge/position pair was already used, skipping.')
-                #     append_if_not_used((edge.dest, i), alternatives, used_values)
-                if string_matches_edge_input(string, i, edge):
-                    logger.debug(f'Matched: "{string[i:i+input_length]}"')
-                    state_pos_pair = (edge.dest, i+input_length)
-                    if state_pos_pair not in used_state_pos:
-                        logger.debug(f'Appending {state_pos_pair} (equivalent to {(edge.dest, string[i+input_length:])})')
-                        alternatives.append(state_pos_pair)
+                if string_path_edge_input(string, i, edge):
+                    self.debug(f'Matched: "{string[i:match_end]}" at position {i} in "{string}"')
+
+                    path_step = StateRecord(self.id, state_id, i, match_end)
+                    self.debug(f'Appending path step {path_step} to path {path}.')
+                    path.append(path_step)
+
+                    destination_state = edge.dest
+                    if (destination_state, match_end) not in used_state_pos:
+                        state_pos_path_tuple = (destination_state, match_end, path)
+                        self.debug(f'Appending {state_pos_path_tuple} (equivalent to {(edge.dest, string[i+input_length:])}) to alternatives {alternatives}.')
+                        assert state_pos_path_tuple[0] in self.state_map.keys(), f'Attempted to append state_pos_path_tuple {state_pos_path_tuple} whose state was not in the state map {self.state_map.keys()} to alternatives list {alternatives}!'
+                        alternatives.append(state_pos_path_tuple)
                     else:
-                        logger.debug(f'Edge/position pair was already used, skipping.')
+                        self.debug(f'Edge/position pair was already used, skipping.')
+                else:
+                    self.debug(f'Edge did not match!')
 
-            if state == FINAL_STATE and i == len(string):
-                logger.debug(f'Found an accepting path.')
-                return True
+            if state_id == FINAL_STATE and (partial_match or i == len(string)):
+                self.debug(f'Found an accepting path: {prev_path}')
+                return True, prev_path, i
                 break
-        return False
+        return False, None, -1
 
-def string_matches_edge_input(string, i, edge):
+def string_path_edge_input(string, i, edge):
     return edge.input == '' or (i <= len(string) - len(edge.input) and edge.input == string[i:i+len(edge.input)])
 
-def append_if_not_used(value, target, used_choices):
-    if value not in used_choices:
-        target.append(value)
 
 if __name__ == '__main__':
     args = set_up_logging.set_up_logging()
--- state_machine_test.py
+++ state_machine_test.py
@@ -2,11 +2,20 @@ import unittest
 import logging
 import sys
 
-from state_machine import StateMachine, State, Edge, START_STATE, FINAL_STATE
+from state_machine import StateMachine, StateRecord, State, Edge, START_STATE, FINAL_STATE
 
 # logging.basicConfig(level=logging.DEBUG)
 
+def gen_test_state_record(id_, start, end):
+    return StateRecord(0, id_, start, end)
+
 class SimpleMachinesTestCase(unittest.TestCase):
+
+    def test_state_record_equality(self):
+        state_record_1 = StateRecord('a', START_STATE, 0, 0)
+        state_record_2 = StateRecord('a', START_STATE, 0, 0)
+
+        self.assertEqual(state_record_1, state_record_2)
 
     # For accepting raw tokens
     def test_accepts_single_symbol(self):
@@ -21,14 +30,18 @@ class SimpleMachinesTestCase(unittest.TestCase):
                         State('b', Edge('b', FINAL_STATE)),
                         State(FINAL_STATE)])
         self.assertTrue(machine.accepts('a'))
+        machine.reset()
         self.assertTrue(machine.accepts('ab'))
+        machine.reset()
         self.assertFalse(machine.accepts('b'))
 
     # For accepting repeated tokens
     def test_accepts_repeated_symbols(self):
         machine = StateMachine(states=[State(START_STATE, Edge('a', FINAL_STATE)), State(FINAL_STATE, Edge('', START_STATE))])
         self.assertTrue(machine.accepts('a'))
+        machine.reset()
         self.assertTrue(machine.accepts('aa'))
+        machine.reset()
         self.assertTrue(machine.accepts('aaa'))
 
     # For accepting N repeated tokens
@@ -40,7 +53,9 @@ class SimpleMachinesTestCase(unittest.TestCase):
                         State(FINAL_STATE)
                 ])
         self.assertFalse(machine.accepts('a'))
+        machine.reset()
         self.assertTrue(machine.accepts('aa'))
+        machine.reset()
         self.assertFalse(machine.accepts('aaa'))
 
     # For accepting N* repeated tokens
@@ -53,8 +68,11 @@ class SimpleMachinesTestCase(unittest.TestCase):
                         State(FINAL_STATE),
                         ])
         self.assertFalse(machine.accepts('a'))
+        machine.reset()
         self.assertFalse(machine.accepts('aa'))
+        machine.reset()
         self.assertTrue(machine.accepts('aaa'))
+        machine.reset()
         self.assertTrue(machine.accepts('aaaa'))
 
     # For accepting *N repeated tokens
@@ -67,8 +85,11 @@ class SimpleMachinesTestCase(unittest.TestCase):
                         State(FINAL_STATE)
                         ])
         self.assertTrue(machine.accepts('a'))
+        machine.reset()
         self.assertTrue(machine.accepts('aa'))
+        machine.reset()
         self.assertTrue(machine.accepts('aaa'))
+        machine.reset()
         self.assertFalse(machine.accepts('aaaa'))
 
     # For accepting A / B tokens
@@ -81,7 +102,172 @@ class SimpleMachinesTestCase(unittest.TestCase):
                         State(FINAL_STATE)
                 ])
         self.assertTrue(machine.accepts('ab'))
+        machine.reset()
         self.assertTrue(machine.accepts('ac'))
+
+class ContinuationTestCases(unittest.TestCase):
+    # For allowing repeated calls to sub-automata
+    def test_continuation_works(self):
+        machine = StateMachine(states=[State(START_STATE, Edge('a', FINAL_STATE)), State(FINAL_STATE, Edge('', START_STATE))])
+        state_record = gen_test_state_record(START_STATE, 0, 1)
+        state_records = [state_record]
+        result = (True, state_records, 1)
+        self.assertEqual(machine.accepts_partial('aaa'), result)
+
+        state_record = gen_test_state_record(START_STATE, 0, 1)
+        end_record = gen_test_state_record(FINAL_STATE, 1, 1)
+        state_record2 = gen_test_state_record(START_STATE, 1, 2)
+        state_records = [state_record, end_record, state_record2]
+        result = (True, state_records, 2)
+        self.assertEqual(machine.accepts_partial('aaa'), result)
+
+        state_record = gen_test_state_record(START_STATE, 0, 1)
+        end_record = gen_test_state_record(FINAL_STATE, 1, 1)
+        state_record2 = gen_test_state_record(START_STATE, 1, 2)
+        end_record2 = gen_test_state_record(FINAL_STATE, 2, 2)
+        state_record3 = gen_test_state_record(START_STATE, 2, 3)
+        state_records = [state_record, end_record, state_record2, end_record2, state_record3]
+        result = (True, state_records, 3)
+        self.assertEqual(machine.accepts_partial('aaa'), result)
+
+        self.assertEqual(machine.accepts_partial('aaa'), (False, None, -1))
+
+    # For allowing repeated calls to sub-automata
+    def test_continuation_with_full_automata_repetition(self):
+        machine = StateMachine(states=[
+                State(START_STATE, Edge('', 'a')),
+                State('a', Edge('a', FINAL_STATE)),
+                State(FINAL_STATE, Edge('', START_STATE))])
+        start_record = gen_test_state_record(START_STATE, 0, 0)
+        a_record = gen_test_state_record('a', 0, 1)
+        state_records = [start_record, a_record]
+        result = (True, state_records, 1)
+        self.assertEqual(machine.accepts_partial('aab'), result)
+
+        start_record = gen_test_state_record(START_STATE, 0, 0)
+        a_record = gen_test_state_record('a', 0, 1)
+        end_record = gen_test_state_record(FINAL_STATE, 1, 1)
+        start_record_2 = gen_test_state_record(START_STATE, 1, 1)
+        a_record_2 = gen_test_state_record('a', 1, 2)
+        state_records = [start_record, a_record, end_record, start_record_2, a_record_2]
+        result = (True, state_records, 2)
+        self.assertEqual(machine.accepts_partial('aab'), result)
+
+        self.assertEqual(machine.accepts_partial('aab'), (False, None, -1))
+
+    # For allowing repeated calls to sub-automata
+    def test_continuation_with_internal_repetition(self):
+        machine = StateMachine(states=[
+                State(START_STATE, Edge('', 'a')),
+                State('a', [Edge('a', 'a'), Edge('', FINAL_STATE)]),
+                State(FINAL_STATE)])
+        start_record = gen_test_state_record(START_STATE, 0, 0)
+        a_record = gen_test_state_record('a', 0, 0)
+        state_records = [start_record, a_record]
+        result = (True, state_records, 0)
+        self.assertEqual(machine.accepts_partial('aab'), result)
+
+        start_record = gen_test_state_record(START_STATE, 0, 0)
+        a_record = gen_test_state_record('a', 0, 1)
+        a_record_2 = gen_test_state_record('a', 1, 1)
+        state_records = [start_record, a_record, a_record_2]
+        result = (True, state_records, 1)
+        self.assertEqual(machine.accepts_partial('aab'), result)
+
+        start_record = gen_test_state_record(START_STATE, 0, 0)
+        a_record = gen_test_state_record('a', 0, 1)
+        a_record_2 = gen_test_state_record('a', 1, 2)
+        a_record_3 = gen_test_state_record('a', 2, 2)
+        state_records = [start_record, a_record, a_record_2, a_record_3]
+        result = (True, state_records, 2)
+        self.assertEqual(machine.accepts_partial('aab'), result)
+
+        self.assertEqual(machine.accepts_partial('aab'), (False, None, -1))
+
+    # For testing the ordering of edge traversal. Since traversal is via DFS,
+    # the internal loop should be preferred over the outside loop. Looks very
+    # similar to the previous test, but validates something different.
+    def test_edge_traversal_order(self):
+        machine = StateMachine(states=[
+                State(START_STATE, Edge('', 'a')),
+                State('a', [Edge('a', 'a'), Edge('', FINAL_STATE)]),
+                State(FINAL_STATE)])
+        start_record = gen_test_state_record(START_STATE, 0, 0)
+        a_record = gen_test_state_record('a', 0, 0)
+        end_record = gen_test_state_record(FINAL_STATE, 0, 0)
+        state_records = [start_record, a_record]
+        result = (True, state_records, 0)
+        self.assertEqual(machine.accepts_partial('aab'), result)
+
+        start_record = gen_test_state_record(START_STATE, 0, 0)
+        a_record = gen_test_state_record('a', 0, 1)
+        a_record_2 = gen_test_state_record('a', 1, 1)
+        state_records = [start_record, a_record, a_record_2]
+        result = (True, state_records, 1)
+        self.assertEqual(machine.accepts_partial('aab'), result)
+
+        start_record = gen_test_state_record(START_STATE, 0, 0)
+        a_record = gen_test_state_record('a', 0, 1)
+        a_record_2 = gen_test_state_record('a', 1, 2)
+        a_record_3 = gen_test_state_record('a', 2, 2)
+        state_records = [start_record, a_record, a_record_2, a_record_3]
+        result = (True, state_records, 2)
+        self.assertEqual(machine.accepts_partial('aab'), result)
+
+        self.assertEqual(machine.accepts_partial('aab'), (False, None, -1))
+
+class SubMachineTestCases(unittest.TestCase):
+    # For ensuring that sub-automata register and run properly.
+    def test_sub_machines(self):
+        machine = StateMachine(id_='a', states=[
+                State(START_STATE, Edge('', 'b')),
+                State('b', Edge('a', FINAL_STATE), is_automata=True),
+                State(FINAL_STATE, Edge('', START_STATE))
+                ])
+        submachine_b = StateMachine(id_='b', states=[
+                State(START_STATE, Edge('b', FINAL_STATE)),
+                State(FINAL_STATE, Edge('', START_STATE))
+                ])
+        machine.register_automata(submachine_b)
+
+        state_record = StateRecord('a', START_STATE, 0, 0)
+        nested_start_record = StateRecord('b', START_STATE, 0, 1)
+        submachine_record = StateRecord('a', 'b_internal', 0, 1, [nested_start_record])
+        b_record = StateRecord('a', 'b', 1, 2)
+        state_records = [state_record, submachine_record, b_record]
+        result = (True, state_records, 2)
+        self.assertEqual(machine.accepts_partial('ba'), result)
+
+    #For ensuring that sub-automata register and run properly.
+    def test_double_nested_sub_machines(self):
+        machine = StateMachine(id_='c', states=[
+                State(START_STATE, Edge('', 'b')),
+                State('b', Edge('c', FINAL_STATE), is_automata=True),
+                State(FINAL_STATE)
+                ])
+        submachine_b = StateMachine(id_='b', states=[
+                State(START_STATE, Edge('', 'a')),
+                State('a', Edge('b', FINAL_STATE), is_automata=True),
+                State(FINAL_STATE)
+                ])
+        submachine_a = StateMachine(id_='a', states=[
+                State(START_STATE, Edge('a', FINAL_STATE)),
+                State(FINAL_STATE)
+                ])
+
+        submachine_b.register_automata(submachine_a)
+        machine.register_automata(submachine_b)
+
+        start_record = StateRecord('c', START_STATE, 0, 0)
+        b_start_record = StateRecord('b', START_STATE, 0, 0)
+        a_record = StateRecord('a', START_STATE, 0, 1)
+        b_machine_record = StateRecord('b', 'a_internal', 0, 1, [a_record])
+        b_record = StateRecord('b', 'a', 1, 2)
+        c_machine_record = StateRecord('c', 'b_internal', 0, 2, [b_start_record, b_machine_record, b_record])
+        c_record = StateRecord('c', 'b', 2, 3)
+        state_records = [start_record, c_machine_record, c_record]
+        result = (True, state_records, 3)
+        self.assertEqual(machine.accepts_partial('abc'), result)
 
 class AStarBRepeatedStateMachineTestCase(unittest.TestCase):
 
